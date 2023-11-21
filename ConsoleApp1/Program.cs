ee4cba85f91abf76c807a36bc3db2a1e31d09af3
Artyom Lyakhov <artemlih16@gmail.com>
2023-11-21 12:27:09 +0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+namespace MathSyntaxTree
+{
+    class Program
+    {
+
+        private readonly Tokenizer _tokenizer;
+        private readonly ShuntingYardAlgorithm _algorithm;
+        private readonly PostfixNotationCalculator _calculator;
+
+        public Program()
+        {
+            _tokenizer = new Tokenizer();
+            _algorithm = new ShuntingYardAlgorithm();
+            _calculator = new PostfixNotationCalculator();
+        }
+
+        static void Main()
+        {
+            Program program = new();
+            string expression = "2*(3+4)+5";
+            Console.WriteLine(program.Calculate(expression));
+        }
+        private double Calculate(string expression)
+        {
+            var infixNotationTokens = _tokenizer.Parse(expression);
+            var postfixNotationTokens = _algorithm.Apply(infixNotationTokens);
+            return _calculator.Calculate(postfixNotationTokens).Value;
+        }
+    }
+}
